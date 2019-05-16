f0be38f3f3ec8cfc274ebd3ae995566fd1d39677
Pavel Iarov <Pavel_Iarov@epam.com>
2019-05-16 17:05:44 +0300

    D4J-78: added action to cancel session
    Issue: D4J-78

--- src/popup.js
+++ src/popup.js
@@ -4,6 +4,7 @@ import { App } from './popup-component';
 import './common/style-guide/common.scss';
 import './common/style-guide/fonts/fonts.scss';
 import { configureAxios } from './common/connection';
+import './large-logo.png';
 import './default-logo.png';
 
 configureAxios().then(() => {
