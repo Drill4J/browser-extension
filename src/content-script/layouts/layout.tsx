3745a78812480c811633fc6cc3dbdf01183b405d
pavyarov <Pavel_Iarov@epam.com>
2020-03-27 13:45:31 +0300

    EPMDJ-2522: fix stackoverflow issue at drag&drop
    Issue: EPMDJ-2522

--- src/content-script/layouts/layout.tsx
+++ src/content-script/layouts/layout.tsx
@@ -1,10 +1,6 @@
 import * as React from 'react';
 import { BEM } from '@redneckz/react-bem-helper';
 import Draggable, { DraggableEventHandler } from 'react-draggable';
-import { browser } from 'webextension-polyfill-ts';
-
-import { useDispatcher } from '../hooks';
-import { savePosition } from '../reducer';
 
 import styles from './layout.module.scss';
 
@@ -14,44 +10,37 @@ interface Props {
   header?: React.ReactNode;
   sidebar?: React.ReactNode;
   position?: { x: number; y: number };
+  onPositionChange: DraggableEventHandler;
 }
 
 const layout = BEM(styles);
 
 export const Layout = layout(({
-  className, sidebar, children, header, position,
-}: Props) => {
-  const dispatcher = useDispatcher();
-  const handleStop: DraggableEventHandler = (e, { x, y }) => {
-    browser.storage.local.set({ position: { x, y } });
-    dispatcher(savePosition({ x, y }));
-  };
-
-  return (
-    <Draggable
-      bounds="body"
-      handle=".drag-wrapper"
-      onStop={handleStop}
-      position={position}
-    >
-      <div className={className}>
-        <Header>
-          {header}
-        </Header>
-        <ContentWrapper>
-          <Sidebar>
-            {sidebar}
-          </Sidebar>
-          {Boolean(children) && (
-            <Content>
-              {children}
-            </Content>
-          )}
-        </ContentWrapper>
-      </div>
-    </Draggable>
-  );
-});
+  className, sidebar, children, header, position, onPositionChange,
+}: Props) => (
+  <Draggable
+    bounds="body"
+    handle=".drag-wrapper"
+    onStop={onPositionChange}
+    position={position}
+  >
+    <div className={className}>
+      <Header>
+        {header}
+      </Header>
+      <ContentWrapper>
+        <Sidebar>
+          {sidebar}
+        </Sidebar>
+        {Boolean(children) && (
+          <Content>
+            {children}
+          </Content>
+        )}
+      </ContentWrapper>
+    </div>
+  </Draggable>
+));
 
 const Header = layout.header('div');
 const Sidebar = layout.sidebar('div');
