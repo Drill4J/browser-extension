019f702aacecaec3b5ed259004dcf4664e9296d8
pavyarov <Pavel_Iarov@epam.com>
2020-03-31 15:17:11 +0300

    EPMDJ-2538: change set agent cofig from url to request initiator
    Issue: EPMDJ-2538

--- src/background.ts
+++ src/background.ts
@@ -16,8 +16,8 @@ browser.storage.onChanged.addListener(() => {
   });
 });
 
-function requestInteceptor({ requestHeaders = [], url }: WebRequest.OnBeforeSendHeadersDetailsType) {
-  const requestHost = getHost(url);
+function requestInteceptor({ requestHeaders = [], initiator = '' }: WebRequest.OnBeforeSendHeadersDetailsType & { initiator?: string}) {
+  const requestHost = getHost(initiator);
   const { sessionId, testName, isActive } = configMap[requestHost] || {};
   if (isActive) {
     requestHeaders.push({ name: 'drill-session-id', value: sessionId });
@@ -46,12 +46,12 @@ const DRILL_RESPONSE_HEADERS = ['drill-group-id', 'drill-agent-id', 'drill-admin
 
 const DRILL_REQUEST_HEADER = ['drill-session-id', 'drill-test-name'];
 
-function responseInterceptor({ responseHeaders = [], url }: WebRequest.OnHeadersReceivedDetailsType) {
+function responseInterceptor({ responseHeaders = [], initiator = '' }: WebRequest.OnHeadersReceivedDetailsType & { initiator?: string }) {
   const drillHeaders = responseHeaders.filter(
     ({ name }) => DRILL_RESPONSE_HEADERS.includes(name),
   ).reduce((acc, { name, value }) => ({ ...acc, [toCamel(name)]: value }), {});
 
-  Object.keys(drillHeaders).length && storeConfig(getHost(url), drillHeaders);
+  Object.keys(drillHeaders).length && storeConfig(getHost(initiator), drillHeaders);
 }
 
 async function storeConfig(host: string, config?: { [key: string]: string | undefined }) {
