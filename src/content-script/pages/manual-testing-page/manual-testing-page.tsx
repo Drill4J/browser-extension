c9701ea9badddd037aa297553d97581ef5829db3
pavyarov <Pavel_Iarov@epam.com>
2020-04-29 11:59:47 +0300

    added status hande after redirect from sidebar

--- src/content-script/pages/manual-testing-page/manual-testing-page.tsx
+++ src/content-script/pages/manual-testing-page/manual-testing-page.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
-import { Switch, Route, useHistory } from 'react-router-dom';
-import { browser } from 'webextension-polyfill-ts';
+import {
+  Switch, Route, useHistory,
+} from 'react-router-dom';
 
 import { useAgentConfig, useAgentInfo } from '../../../hooks';
 
@@ -10,18 +11,17 @@ import { FinishRecording } from './finish-recording';
 import { UnavailablePage } from '../unavailable-page';
 
 export const ManualTestingPage = () => {
-  const { push } = useHistory();
+  const { push, location: { pathname } } = useHistory();
   const config = useAgentConfig();
   const { status = '' } = useAgentInfo(config?.drillAdminUrl, config?.drillAgentId) || {};
 
   React.useEffect(() => {
-    if (status === 'BUSY' || status === 'OFFLINE') {
+    if ((status === 'BUSY' || status === 'OFFLINE') && pathname === '/manual-testing') {
       push('/unavailable-page');
-      return;
+    } else if (pathname !== '/unavailable-page' && pathname !== '/test-to-code') {
+      config?.isActive ? push('/manual-testing/in-progress') : push('/manual-testing');
     }
-
-    config?.isActive ? push('/manual-testing/in-progress') : push('/manual-testing');
-  }, [config, status]);
+  }, [config, status, pathname]);
 
   return (
     <Switch>
