4c4b6f8583210644758a0cdf58e1d09c49544582
RomanDavlyatshin <davlyatshinra@gmail.com>
2021-02-12 18:45:31 +0400

    fix: options page regex

--- src/options-script/app.tsx
+++ src/options-script/app.tsx
@@ -27,8 +27,9 @@ const validateDomain = composeValidators(
   required('backendAddress', 'Admin URL'),
   correctPattern(
     'backendAddress',
-    /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)*:\d{4}/gm,
-    'Admin API URL is not correct. Please enter a valid URL matching the "http(s)://host:port”',
+    // eslint-disable-next-line max-len
+    /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)*:(6553[0-5]|655[0-2][0-9]|65[0-4][0-9][0-9]|6[0-4][0-9][0-9][0-9][0-9]|[1-5](\d){4}|[1-9](\d){0,3})$/,
+    'Admin API URL is not correct. Please enter a valid URL matching the "http(s)://host:port"',
   ),
 );
 
