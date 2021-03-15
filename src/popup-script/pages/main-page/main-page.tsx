20b26c3925aa8575032966a88c2bbc19227bb1ad
Ilia_Solovev1 <49729985+IliaSolovev@users.noreply.github.com>
2021-03-15 10:56:10 +0300

    fix: Remoe unneccessary block

--- src/popup-script/pages/main-page/main-page.tsx
+++ src/popup-script/pages/main-page/main-page.tsx
@@ -31,59 +31,57 @@ export const MainPage = mainPage(({ className, agent }: Props) => {
   const isWidgetVisible = hostStorage?.isWidgetVisible;
   return (
     <div className={className}>
-      <div className="h-100">
-        <Header className="d-flex align-items-center px-4 gx-2">
-          <Logo viewBox="0 0 16 16" width={24} height={16} style={{ minWidth: '24px' }} />
-          <div title={agent?.id} className="text-ellipsis fs-14 lh-20 bold">{agent?.id}</div>
-          <AgentStatus className="ml-auto" status={status} />
-        </Header>
-        <div className="d-flex flex-column justify-content-center align-items-center h-100 px-4 gy-6">
-          <span className="text-center">
-            {status === Status.ONLINE && (
-              <span>
-                You&#39;re all set now.&nbsp;
-                { isWidgetVisible
-                  ? 'You can use the widget \n for manual testing.'
-                  : 'Click "Open Widget" \n to start manual testing.' }
-              </span>
-            )}
-            {status === Status.NOT_REGISTERED && (
-              <span>Agent is not registered. Please complete registration in the Admin Panel</span>
-            )}
-            {(status === Status.OFFLINE || status === Status.BUSY) && (
-              <span>
-                Agent is currently&nbsp;
-                {status.toLowerCase()}
-                . In order to perform testing the agent has to be online.
-              </span>
-            )}
-          </span>
-          { !isWidgetVisible ? (
-            <Button
-              className="mx-auto"
-              type="primary"
-              size="large"
-              disabled={status !== Status.ONLINE}
-              onClick={
-                async () => {
-                  await browser.storage.local.set({ [host]: { ...hostStorage, isWidgetVisible: true } });
-                  injectContentScript(activeTab);
-                }
-              }
-            >
-              Open Widget
-            </Button>
-          ) : (
-            <Button
-              className="mx-auto"
-              type="secondary"
-              size="large"
-              onClick={() => browser.storage.local.set({ [host]: { ...hostStorage, isWidgetVisible: false } })}
-            >
-              Hide Widget
-            </Button>
+      <Header className="d-flex align-items-center px-4 gx-2">
+        <Logo viewBox="0 0 16 16" width={24} height={16} style={{ minWidth: '24px' }} />
+        <div title={agent?.id} className="text-ellipsis fs-14 lh-20 bold">{agent?.id}</div>
+        <AgentStatus className="ml-auto" status={status} />
+      </Header>
+      <div className="d-flex flex-column justify-content-center align-items-center h-100 px-4 gy-6">
+        <span className="text-center">
+          {status === Status.ONLINE && (
+            <span>
+              You&#39;re all set now.&nbsp;
+              { isWidgetVisible
+                ? 'You can use the widget \n for manual testing.'
+                : 'Click "Open Widget" \n to start manual testing.' }
+            </span>
           )}
-        </div>
+          {status === Status.NOT_REGISTERED && (
+            <span>Agent is not registered. Please complete registration in the Admin Panel</span>
+          )}
+          {(status === Status.OFFLINE || status === Status.BUSY) && (
+            <span>
+              Agent is currently&nbsp;
+              {status.toLowerCase()}
+              . In order to perform testing the agent has to be online.
+            </span>
+          )}
+        </span>
+        { !isWidgetVisible ? (
+          <Button
+            className="mx-auto"
+            type="primary"
+            size="large"
+            disabled={status !== Status.ONLINE}
+            onClick={
+              async () => {
+                await browser.storage.local.set({ [host]: { ...hostStorage, isWidgetVisible: true } });
+                injectContentScript(activeTab);
+              }
+            }
+          >
+            Open Widget
+          </Button>
+        ) : (
+          <Button
+            className="mx-auto"
+            type="secondary"
+            size="large"
+            onClick={() => browser.storage.local.set({ [host]: { ...hostStorage, isWidgetVisible: false } })}
+          >
+            Hide Widget
+          </Button>
+        )}
       </div>
     </div>
   );
